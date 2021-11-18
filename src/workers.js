49131872840e05685c3421d406d2767f32bce84f
eliransiboni <eliran.siboni@gmail.com>
2021-11-18 14:43:26 +0000

    Add datastore and images

---
+++ src/workers.js
@@ -0,0 +1,57 @@
+export const workers = [
+  {
+    id: 1,
+    name: "Tanoai Reed",
+    image: process.env.PUBLIC_URL + "/images/tanoai_reed.webp",
+    movies: [
+      {
+        title: "Pain & Gain",
+        actor: "Dwayne Johnson",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Lee Chesley",
+    image: process.env.PUBLIC_URL + "/images/lee_chesley.jpeg",
+    movies: [
+      {
+        title: "Jumanji: The Next Level",
+        actor: "Awkwafina",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Jacob Tomuri",
+    image: process.env.PUBLIC_URL + "/images/jacob_tomuri.jpeg",
+    movies: [
+      {
+        title: "Mad Max: Fury Road",
+        actor: "Tom Hardy",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Aline Mayne",
+    image: process.env.PUBLIC_URL + "/images/aline_mayne.webp",
+    movies: [
+      {
+        title: "The Undoing",
+        actor: "Nicole Kidman",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Anisha Tee Gibbs",
+    image: process.env.PUBLIC_URL + "/images/anisha_tee_gibbs.jpeg",
+    movies: [
+      {
+        title: "John Wick: Chapter 3 — Parabellum",
+        actor: "Halle Berry",
+      },
+    ],
+  },
+];
