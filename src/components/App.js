ed589d33e0400afc85c1a2c7aff30630b17466e2
eliransiboni <eliran.siboni@gmail.com>
2021-11-19 17:56:34 +0000

    Add Header to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import CardStack from "./CardStack";
 
 import "../styles/App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <CardStack />
     </div>
   );
