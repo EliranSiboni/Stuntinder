ef4d0494836eb0764d5a0106776484af90e00424
eliransiboni <eliran.siboni@gmail.com>
2021-11-21 17:29:57 +0000

    Remove unused line

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,6 @@ function App() {
     <Router>
       <div className="App">
         <Header />
-
         <Routes>
           <Route path="/" element={<CardStack />} />
           <Route path="stunts" element={<Stunts />} />
