610ada9edff5e694514cd60454254b5e246e26f1
eliransiboni <eliran.siboni@gmail.com>
2021-11-21 19:11:22 +0000

    Add workers load test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import App from "./components/App";
+import { workers } from "./workers";
 
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("get workers file", () => {
+  expect(workers.length).toBeGreaterThanOrEqual(0);
 });
